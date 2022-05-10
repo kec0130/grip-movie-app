2643064a3cb7a5adbcb5905904d5180fb11ef81f
Eunchae Ko <kec0130@gmail.com>
2022-05-10 15:10:46 +0900

    Add basic structures

---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import Movies from './Movies'
+import styles from './Routes.module.scss'
+
+const App = () => {
+  return (
+    <div className={styles.app}>
+      <Movies />
+    </div>
+  )
+}
+
+export default App
