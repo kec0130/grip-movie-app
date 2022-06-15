ffd7662193d626432e3812e30c7097d3061ce8c6
Eunchae Ko <kec0130@gmail.com>
2022-06-16 01:35:10 +0900

    Feat: Replace 404 page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from 'react-router-dom'
 
 import Layout from 'components/Layout'
+import Error from 'components/Error'
 import Movies from './Movies'
 import Favorites from './Favorites'
-import NotFound from './NotFound'
 
 const App = () => {
   return (
@@ -12,7 +12,7 @@ const App = () => {
         <Route path='/' element={<Movies />} />
         <Route path='favorites' element={<Favorites />} />
       </Route>
-      <Route path='*' element={<NotFound />} />
+      <Route path='*' element={<Error message='페이지를 찾을 수 없습니다.' />} />
     </Routes>
   )
 }
