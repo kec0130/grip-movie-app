d9d3cba036c2fa31f31ce1d04376490f0c0c4a25
Eunchae Ko <kec0130@gmail.com>
2022-06-16 01:19:17 +0900

    Feat: Add error component

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,4 +1,4 @@
-export { ReactComponent as LogoImage } from './logo.svg'
 export { ReactComponent as SearchIcon } from './search.svg'
 export { ReactComponent as HeartFillIcon } from './favorite.svg'
 export { ReactComponent as HeartBorderIcon } from './favorite_border.svg'
+export { ReactComponent as ErrorIcon } from './error.svg'
